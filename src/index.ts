62feb9e2a67362e1fcbfae948fe8b4eed4ea66b8
Evgeniy Ostapenko <eug.ostap@gmail.com>
2021-08-18 17:32:16 +0300

    правки

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
 import { Command } from 'commander';
 import { check, getInfo, updateLocales } from './main';
@@ -19,17 +19,17 @@ programm
   .action(check);
 
 // programm
-//   .command('update')
-//   .description('Обновление структуры файла с указанием источника')
-//   .argument('<source>', 'Файл источник')
-  // .requiredOption('-t, --target <value>', 'Файл/директория файлов локализации')
-  // .option('-c, --clean', 'Удалить все несуществующие ключи перевода')
-  // .option('-q, --quiet', 'Исполнение без вывода в консоль')
-  // .action(updateLocales);
+//  .command('update')
+//  .description('Обновление структуры файла с указанием источника')
+//  .argument('<source>', 'Файл источник')
+// .requiredOption('-t, --target <value>', 'Файл/директория файлов локализации')
+// .option('-c, --clean', 'Удалить все несуществующие ключи перевода')
+// .option('-q, --quiet', 'Исполнение без вывода в консоль')
+// .action(updateLocales);
 
 programm
   .command('info')
-  .description('')
+  .description('Поиск строк на кириллице')
   .argument('<source...>', 'Файлы для проверки')
   .option('-d, --debug', 'Флаг включения debug режима')
   .action(getInfo);
